544c4cb455be2af597b15ca93ce911516decbd8b
Ivan <ivan@plugion.org>
2021-04-14 16:41:08 +0300

    Update example.js

--- example.js
+++ example.js
@@ -3,8 +3,11 @@ jQuery(document).on( 'wbk_on_form_rendered', function(){
     jQuery('.wpcf7-text').change( function(){
         if( jQuery('#wbk-email').val() == jQuery('#wbk-email-confirm').val()){
             jQuery('#wbk-book_appointment').prop('disabled', false);
+            jQuery('.email_confirm_error').remove();
         } else {
             jQuery('#wbk-book_appointment').prop('disabled', true);
+            jQuery('.email_confirm_error').remove();
+            jQuery('#wbk-email-confirm').after('<p class="email_confirm_error" style="color:red;">E-mails are different</p>');
         }
     });
 });
